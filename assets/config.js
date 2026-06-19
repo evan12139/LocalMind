357782e304420975de9d036462e0661e79e1062b
evan <1213922281@qq.com>
2026-06-19 10:08:24 +0800

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 // 上线前只需要改这一行。
 // 方案 A：把安装包放到本页面同级的 downloads 文件夹，保持下面这个相对地址。
 // 方案 B：换成腾讯云 COS/对象存储直链，要求免登录、无广告、点击即下载。
-window.LOCALMIND_DOWNLOAD_URL =
+window.LOCALMIND_DOWNLOAD_URL=
 "https://github.com/evan12139/localmind-download/releases/download/v1.0.0/LocalMind-1.0.0-setup.exe";
